1f2998fb0ba373f023d8c5de41fd8db494e4d609
Muhammad Ashhad <162056028+Rukhsanaashhad@users.noreply.github.com>
2025-04-29 23:38:53 +0500

    main.py

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-import streamlit as st
-import random
-import string
+import streamlit as st  
+import random  
+import string  
 
-def generate_password_meter(length ,use_digits  ,use_special):
-    characters = string.ascii_letters
+def generate_password_meter(length, use_digits, use_special):  
+    characters = string.ascii_letters  
 
+    if use_digits:  
+        characters += string.digits  
 
-    if use_digits:
-        characters += string.digits
+    if use_special:  
+        characters += string.punctuation  
 
-    if use_special:
-        characters += string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))  
 
-    return ''.join(random.choice(characters) for loop in range(length))
+st.title("Password Generator By Muhammad Ashhad Khan")  
 
-st.title("Password Generator By Muhammad Ashhad Khan")
+length = st.slider("Select Password Length", min_value=8, max_value=20, value=12)  
 
-length = st.slider("Select Password Length", min_value=8 , max_value=20, value=12)
+use_digits = st.checkbox("Include Digits")  
+use_special = st.checkbox("Use Special Characters")  
 
-use_digits = st.checkbox("Include Digits")
+if st.button("Generate Password"):  
+    password = generate_password_meter(length, use_digits, use_special)  
+    st.write(f"Generated Password: `{password}`")  
 
-use_special = st.checkbox("Use Special Characters")
-
-if st.button("Generate Password"):
-    password = generate_password_meter(use_digits  ,use_special)
-    st.write(f"Generated Password: `{password}`")
-
-    st.write("---------------------")
-
-    st.write("Created By [Muhammad Ashhad Khan](https://github.com/Rukhsanaashhad)")
+    st.write("---------------------")  
+    st.write("Created By [Muhammad Ashhad Khan](https://github.com/Rukhsanaashhad)")  
